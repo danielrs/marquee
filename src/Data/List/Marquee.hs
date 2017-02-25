da5708d0da89fe206c1e7c9a572c4f6868a851fe
Daniel Rivas <ers.daniel+dev@gmail.com>
2017-02-24 23:58:54 -0600

    Better naming

--- src/Data/List/Marquee.hs
+++ src/Data/List/Marquee.hs
@@ -3,5 +3,4 @@ module Data.List.Marquee where
 import Data.Maybe (fromMaybe)
 
 lookupOr :: (Eq key) => a -> key -> [(key, a)] -> a
-lookupOr backup key = fromMaybe backup . lookup key
-
+lookupOr d key = fromMaybe d . lookup key
